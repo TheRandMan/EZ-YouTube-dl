00402d9ad456202507d8f919d5d3c26dfd0b2f23
TheRandMan <33759959+TheRandMan@users.noreply.github.com>
2021-05-12 19:09:38 -0700

    Update ez-youtube-dl.sh

--- ez-youtube-dl.sh
+++ ez-youtube-dl.sh
@@ -9,7 +9,15 @@ function download_mp4() {
 	# download video with highest available quality, save as mp4
 	clear
 	echo "Downloading video as MP4..."
+	echo
 	youtube-dl -f best "$URL"
+	clear
+	echo
+	echo "Video downloaded."
+	echo
+	echo "Exiting..."
+	echo
+	echo
 	exit
 }
 
@@ -17,13 +25,22 @@ function download_mp3() {
 	# download video with highest available audio quality, extract the audio, save as mp3
 	clear
 	echo "Downloading video as MP3..."
+	echo
 	youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 "$URL"
+	clear
+	echo
+	echo "Audio downloaded."
+	echo
+	echo "Exiting..."
+	echo
+	echo
 	exit
 }
 
 menu(){
 	echo -ne "
-	Download as mp4 or mp3?
+	Download as MP4 or MP3?
+	
 	1) MP4 (video)
 	2) MP3 (audio only)
 	3) Exit 
